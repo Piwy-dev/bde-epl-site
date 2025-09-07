44f60d790cbf650ba156a28a402c1747d3eb5fd9
Pierre-Yves Haubruge <pierre-yves.haubruge@student.uclouvain.be>
2025-09-07 17:59:32 +0200

    top navbar

--- app/__init__.py
+++ app/__init__.py
@@ -27,19 +27,7 @@ def create_app(test_config=None):
         return render_template('/home.html')
     
     @app.route("/members")
-    def membres():
+    def members():
         return render_template('/members.html')
-    
-    @app.route("/privacy")
-    def privacy():
-        return render_template('/privacy.html')
-    
-    @app.route("/terms")
-    def terms():
-        return render_template('/terms.html')
-    
-    @app.route("/suppression-request")
-    def suppression_request():
-        return render_template('/suppression-request.html')
 
     return app
